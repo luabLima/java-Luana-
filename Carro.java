7bb6c5f3e2a075ebd68b6968f11b24d039eb1da6
luabLima <146018738+luabLima@users.noreply.github.com>
2024-02-05 17:51:36 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    public void ligar() {
+        System.out.println("Vrum vrum");
+    }
+
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        meuCarro.ligar();
+    }
+}
+
