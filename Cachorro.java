8828275e980c8cc523002d405552804976a77d75
luabLima <146018738+luabLima@users.noreply.github.com>
2024-02-05 17:53:23 -0300

    Add files via upload

---
+++ Cachorro.java
@@ -0,0 +1,11 @@
+public class Cachorro {
+    public void latir() {
+        System.out.println("Au au");
+    }
+
+    public static void main(String[] args) {
+        Cachorro meuCachorro = new Cachorro();
+        meuCachorro.latir();
+    }
+}
+
