0a016aa5ac884ee478744d09407a7fc04a926bb7
luabLima <146018738+luabLima@users.noreply.github.com>
2024-02-08 09:38:06 -0300

    Add files via upload

---
+++ Numero.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Numero {
+    // Método para verificar se o número é ímpar
+    public static boolean eImpar(int n) {
+        return n % 2 != 0;
+    }
+
+    // Método para verificar se o número é positivo
+    public static boolean ePositivo(int n) {
+        return n > 0;
+    }
+}
+
+ class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Insira um número inteiro: ");
+        int numero1 = scanner.nextInt();
+
+        if (Numero.eImpar(numero1)) {
+            System.out.println(numero1 + " é ímpar.");
+        } else {
+            System.out.println(numero1 + " não é ímpar.");
+        }
+
+        
+        System.out.print("Insira outro número inteiro: ");
+        int numero2 = scanner.nextInt();
+
+        
+        if (Numero.ePositivo(numero2)) {
+            System.out.println(numero2 + " é positivo.");
+        } else {
+            System.out.println(numero2 + " não é positivo.");
+        }
+
+        
+        scanner.close();
+    }
+}
